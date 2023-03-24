7478c77cbaac3719878cde9182b92ea22affbb67
hrishi-19 <hrishakamath.com>
2023-03-24 18:34:01 +0530

    mobile justify

--- src/components/PriceText.jsx
+++ src/components/PriceText.jsx
@@ -9,7 +9,7 @@ const PriceText = () => {
                 <p className='text-4xl leading-10  sm:text-justify text-center font-extrabold w-3/4'>Invest in skills,<br /><span className='text-blue-700/90'>earn 10X</span> of what you paid.</p>
                 <p className='text-md w-3/4 mt-5 text-black/70 font-bold sm:text-justify text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas provident quam consectetur veniam delectus aliquid ex amet ipsam perferendis repellat!</p>
             </div>
-            <div className='sm:w-3/4 w-full  flex flex-wrap  px-2 justify-between sm:items-center items-start py-12'>
+            <div className='sm:w-3/4 w-full  flex flex-wrap  px-2 sm:justify-between justify-evenly sm:items-center items-start py-12'>
                 {
                     grayBox.map((item, index) => <GrayBox key={index} mainText={item.maintext} subText={item.subText} />)
                 }
